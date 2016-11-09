049205c639d9f537ecadb96c9f9636c879eed773
Jairo Martinez Garrido <Ylevendnc@gmail.com>
2016-11-09 18:25:15 +0100

    Prefab de moneda
    Moneda que al iniciarse se lanza aleatoriamente, si choca con el player
    desaparece, y si no, a los 3 segundos desaparece.

---
+++ Assets/Scripts/Moneda.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moneda : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 3);
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2(Random.Range (-10,10),10)*20);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D col){
+
+		if (col.gameObject.tag == "Player"){
+			Destroy (gameObject, 1);
+		}
+	}
+}
