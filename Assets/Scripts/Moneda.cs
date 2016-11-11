394e0d440193e1101524a0a9c41da1d021e6f273
BlckShazam <Ylevendnc@gmail.com>
2016-11-11 17:31:45 +0100

    Control Monedas
    Añadimos contador de monedas, suma de monedas al colisionar, boton de
    reset de la cuenta de monedas y boton de suma de monedas.

--- Assets/Scripts/Moneda.cs
+++ Assets/Scripts/Moneda.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 
 public class Moneda : MonoBehaviour {
+	public  int suma;
 	private Rigidbody2D rb;
+	GameObject txt_moneda;
+	ControlMonedas ctrl_moneda; //accedemos a otro script que ya hemos creado anteriormente
 
 	// Use this for initialization
 	void Start () {
+		int sumar = 5;
 		Destroy (gameObject, 3);
 		rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce (new Vector2(Random.Range (-10,10),10)*20);
+		rb.AddForce (new Vector2(Random.Range (-100,100),100));
+		txt_moneda = GameObject.Find ("texto_moneda");
+			ctrl_moneda = txt_moneda.GetComponent<ControlMonedas>();
 
 	}
 	
@@ -17,10 +23,15 @@ public class Moneda : MonoBehaviour {
 	
 	}
 
-	void OnTriggerEnter2D (Collider2D col){
+	void OnCollisionEnter2D (Collision2D col){
 
-		if (col.gameObject.tag == "Player"){
-			Destroy (gameObject, 1);
+		if (col.gameObject.tag == "Player") {
+			ctrl_moneda.suma_monedas (suma);
+			Destroy (gameObject);
 		}
 	}
+	/*void OnDestroy() {
+		GameObject.Find ("texto_monedas").GetComponent<ControlMonedas> ().suma_monedas (5);
+		}*/
+
 }
