b6bd272e0033f3c1758e88fcfb40226496a89031
BlckShazam <Ylevendnc@gmail.com>
2016-11-23 18:36:22 +0100

    Limitador golpes a la caja aleatoria
    Se han limitado la cantidad de elementos generados a 3 junto con las
    animaciones de cada vez que se genera un elemento.

--- Assets/Scripts/Generador.cs
+++ Assets/Scripts/Generador.cs
@@ -6,19 +6,32 @@ public class Generador : MonoBehaviour {
 	private GameObject moneda_nueva;
 	private Transform pos_salida;
 	private int numero_moneda = 0;
+	Animator anim;
+	private int Golpeado = 0;
 
 	void Start (){
 		pos_salida = transform.Find ("posicion_salida").transform; //Para buscar un objeto dentro de otro objeto. En sistema parental buscas un Child.
 		Debug.Log("Cantidad de monedas: "+monedas.Length);
+		anim = GetComponent<Animator>();
+
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
 		if (col.gameObject.tag == "Player" && moneda_nueva == null) {
 			numero_moneda = Random.Range (0, monedas.Length); 
 			// generar un RANDOM.RANGE del array usando .Length y especificando el -1 porque cuenta hasta 10 elementos, pero al establecer el 0 como iniciar, habría 11 y se debe restar uno para que sean 10 elementos.
-			moneda_nueva = (GameObject)Instantiate (monedas[numero_moneda],
-				pos_salida.position,
-				transform.rotation);
+
+			
+			if (Golpeado <= 3) {
+				
+				moneda_nueva = (GameObject)Instantiate (monedas [numero_moneda],
+					pos_salida.position,
+					transform.rotation);
+
+				anim.SetBool ("golpea", true);
+
+				Golpeado++;
+			}
 		}
 	}
 
